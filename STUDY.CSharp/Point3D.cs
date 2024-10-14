961232d6b341867b42741bc41c31015153d71e6a
basemkasem <basem.kasem16@gmail.com>
2024-10-14 17:56:27 +0300

    Create Point3D class

---
+++ STUDY.CSharp/Point3D.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace STUDY.CSharp;
+
+public class Point3D
+{
+    public double X { get; private set; }
+    public double Y { get; private set; }
+    public double Z { get; private set; }
+
+    public Point3D() : this(0, 0, 0)
+    {
+        
+    }
+    
+    public Point3D(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"X: {X}, Y: {Y}, Z: {Z}");
+    }
+
+    public void ChangeCoordinates(double newX, double newY, double newZ)
+    {
+        X = newX;
+        Y = newY;
+        Z = newZ;
+    }
+
+    public static bool operator ==(Point3D p1, Point3D p2)
+    {
+        return p1.X == p2.X && p1.Y == p2.Y && p1.Z == p2.Z;
+    }
+
+    public static bool operator !=(Point3D p1, Point3D p2)
+    {
+        return p1.X != p2.X || p1.Y != p2.Y || p1.Z != p2.Z;
+
+    }
+}
---
+++ STUDY.CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using STUDY.CSharp;
+
+Point3D p1 = new Point3D(1, 2, 3);
+Point3D p2 = new Point3D(1, 2, 3);
+
+Console.WriteLine(p1 == p2);
+
+void ChangeStr(string str)
+{
+    str += " -> Added part to the string";
+}
+string str = "Hello World";
+Console.WriteLine(str);
+ChangeStr(str);
+Console.WriteLine(str);
