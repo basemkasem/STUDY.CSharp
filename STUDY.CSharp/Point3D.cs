6df62b9fc8eede9a37dce46871a77dfc258b431a
basemkasem <basem.kasem16@gmail.com>
2024-10-14 19:48:23 +0300

    Create Display overload takes an array of points parameter.

--- STUDY.CSharp/Point3D.cs
+++ STUDY.CSharp/Point3D.cs
@@ -25,6 +25,13 @@ public class Point3D
         Console.WriteLine($"X: {X}, Y: {Y}, Z: {Z}");
     }
 
+    public static void Display(Point3D[] points)
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Console.WriteLine($"Point {i+1} => X: {points[i].X}, Y: {points[i].Y}, Z: {points[i].Z}");
+        }
+    }
     public void ChangeCoordinates(double newX, double newY, double newZ)
     {
         X = newX;
@@ -50,8 +57,14 @@ public class Point3D
 
     public override bool Equals(object obj)
     {
-        Point3D p = (Point3D)obj;
-        return X == p.X && Y == p.Y && Z == p.Z;
+        if(ReferenceEquals(this, obj)) return true;
+        if (obj is Point3D)
+        {
+            Point3D p = (Point3D)obj;
+            return X == p.X && Y == p.Y && Z == p.Z;
+        }
+        
+        return false;
     }
     
 }
--- STUDY.CSharp/Program.cs
+++ STUDY.CSharp/Program.cs
@@ -2,15 +2,19 @@
 using STUDY.CSharp;
 
 Point3D p1 = new Point3D(1, 2, 3);
-Point3D p2 = new Point3D(1, 2, 3);
+Point3D p2 = new Point3D(1, 6, 3);
 
-Console.WriteLine(p1 == p2);
+p1.Display();
+Point3D.Display([p1, p2]);
+// Console.WriteLine(p1 == p2);
 
-void ChangeStr(string str)
-{
-    str += " -> Added part to the string";
-}
-string str = "Hello World";
-Console.WriteLine(str);
-ChangeStr(str);
-Console.WriteLine(str);
+// Console.WriteLine(p1.Equals(p2));
+
+// void ChangeStr(string str)
+// {
+//     str += " -> Added part to the string";
+// }
+// string str = "Hello World";
+// Console.WriteLine(str);
+// ChangeStr(str);
+// Console.WriteLine(str);
