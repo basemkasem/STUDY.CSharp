b0f5264506fe35c05a3f7aa9e921561e23c135e9
basemkasem <basem.kasem16@gmail.com>
2024-10-16 21:43:04 +0300

    Update Employee struct to be a class

--- STUDY.CSharp/Employee.cs
+++ STUDY.CSharp/Employee.cs
@@ -2,31 +2,42 @@ using System;
 
 namespace STUDY.CSharp;
 
-public enum Role
+// public enum Role
+// {
+//     Admin = 1,
+//     Accountant = 2,
+//     Stock = 4,
+//     Reception = 8
+// }
+// public struct Employee
+// {
+//     public string Name { get; set; }
+//     public double Salary { get; set; }
+//     public Role EmployeeRole { get; private set; }
+//
+//     public void Display()
+//     {
+//         Console.WriteLine($"Name: {Name}, Salary: {Salary}, Role: {EmployeeRole}");
+//     }
+//
+//     public void AssignRole(Role role)
+//     {
+//         EmployeeRole = role;
+//     }
+//
+//     public bool HasRole(Role role)
+//     {
+//         return EmployeeRole == role;
+//     }
+// }
+
+class Employee : Person
 {
-    Admin = 1,
-    Accountant = 2,
-    Stock = 4,
-    Reception = 8
-}
-public struct Employee
-{
-    public string Name { get; set; }
+    public string? Nid { get; set; }
     public double Salary { get; set; }
-    public Role EmployeeRole { get; private set; }
-
-    public void Display()
+    
+    public override string ToString()
     {
-        Console.WriteLine($"Name: {Name}, Salary: {Salary}, Role: {EmployeeRole}");
-    }
-
-    public void AssignRole(Role role)
-    {
-        EmployeeRole = role;
-    }
-
-    public bool HasRole(Role role)
-    {
-        return EmployeeRole == role;
+        return $"Nid: {Nid}, Salary: {Salary}";
     }
 }
