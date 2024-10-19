4e0b3aa52541d5b3b304808cb5bacf9de4d01399
basemkasem <basem.kasem16@gmail.com>
2024-10-19 23:47:34 +0300

    Implement IComparer interface to trainee class

--- STUDY.CSharp/Trainee.cs
+++ STUDY.CSharp/Trainee.cs
@@ -1,12 +1,37 @@
 namespace STUDY.CSharp;
 
-public class Trainee: Person
+public class Trainee: Person, IComparer<Trainee>, IPayable
 {
     public string? Nid { get; set; }
     public int IntakeNumber { get; set; }
+    public double Salary { get; private set; }
 
+    public Trainee() { }
+
+    public Trainee(string? nid, int intakeNumber, double salary)
+    {
+        Nid = nid;
+        IntakeNumber = intakeNumber;
+        Salary = salary;
+    }
     public override string ToString()
     {
         return $"Nid: {Nid}, Intake Number: {IntakeNumber}";
+    }
+    
+    public void ShowPayment()
+    {
+        Console.WriteLine($"Salary: {Salary}");
+    }
+
+    public int Compare(Trainee t1, Trainee t2)
+    {
+        return t1.Nid.CompareTo(t2.Nid); 
+    }
+    
+    public void Print()
+    {
+            Console.WriteLine($"Nid: {Nid}, IntakeNumber: {IntakeNumber}, Salary: {Salary}");
+
     }
 }
