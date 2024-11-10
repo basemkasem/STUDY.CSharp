8f6ca88e0160e340df5ceb264c532f01f25ac259
basemkasem <basem.kasem16@gmail.com>
2024-11-10 14:25:02 +0200

    Fix indexers logic in phoneBook class

--- STUDY.CSharp/PhoneBook.cs
+++ STUDY.CSharp/PhoneBook.cs
@@ -2,49 +2,57 @@ namespace STUDY.CSharp;
 
 public class PhoneBook
 {
+
+    private Dictionary<string, int> _phoneBook;
+
+    public PhoneBook()
+    {
+        _phoneBook = new();
+    }
     
-    public Dictionary<int, string> phoneBook = new();
-    
+    public int this[string name]
+    {
+        get => _phoneBook.ContainsKey(name) ? _phoneBook[name] : throw new KeyNotFoundException();
+        set
+        {
+            if (_phoneBook.ContainsKey(name))
+                _phoneBook[name]= value;
+            else 
+                throw new KeyNotFoundException();
+        }
+    }
+
     public string this[int number]
     {
         get
         {
-            return phoneBook.ContainsKey(number) ? phoneBook[number] : "Not Found";
+            return _phoneBook.FirstOrDefault(x => x.Value == number).Key;
         }
         set
         {
-            phoneBook[number]= value;
-        }
-    }
-
-    public int this[string name]
-    {
-        get
-        {
-            return  phoneBook.FirstOrDefault(x => x.Value == name).Key;
-        }
-        set
-        {
-            
-            var myKey = phoneBook.FirstOrDefault(x => x.Value == name).Key;
-            if (!phoneBook.ContainsKey(myKey))
+            var myKey = _phoneBook.FirstOrDefault(x => x.Value == number).Key;
+            if (!_phoneBook.ContainsKey(myKey))
             {
-                phoneBook.Add(value,name);
+                throw new IndexOutOfRangeException();
             }
             else
             {
-                var myValue = phoneBook?[myKey];
-                phoneBook.Remove(myKey);
-                phoneBook.Add(myKey, myValue);
+                var myValue = _phoneBook[myKey];
+                _phoneBook.Remove(myKey);
+                _phoneBook.Add(value, myValue);
             }
         }
     }
 
+    public void Add(string name, int number)
+    {
+        _phoneBook.Add(name, number);
+    }
     public void Display()
     {
-        for (int i = 0; i < phoneBook.Count; i++)
+        for (int i = 0; i < _phoneBook.Count; i++)
         {
-            Console.WriteLine(phoneBook.ElementAt(i));
+            Console.WriteLine(_phoneBook.ElementAt(i));
         }
     }
 }
